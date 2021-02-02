f1492d64b0657671235e7e6b07b69c22354b64b6
Marc-Antoine Ferland <maferland@hey.com>
2021-02-01 22:34:06 -0500

    refactor: improve screenshot generation

--- functions/generate.js
+++ functions/generate.js
@@ -1,6 +1,20 @@
 const vibrant = require('node-vibrant')
-const nodeHtmlToImage = require('node-html-to-image')
-const fetch = require('node-fetch')
+const captureWebsite = require('capture-website')
+const fs = require('fs')
+
+// https://stackoverflow.com/questions/6122571/simple-non-secure-hash-function-for-javascript
+String.prototype.hashCode = function () {
+  var hash = 0
+  if (this.length == 0) {
+    return hash
+  }
+  for (var i = 0; i < this.length; i++) {
+    var char = this.charCodeAt(i)
+    hash = (hash << 5) - hash + char
+    hash = hash & hash // Convert to 32bit integer
+  }
+  return Math.abs(hash)
+}
 
 const headers = {
   'Access-Control-Allow-Origin': '*',
@@ -11,14 +25,16 @@ const headers = {
 exports.handler = async (event, context) => {
   try {
     const {url} = JSON.parse(event.body)
-    const html = await fetch(url).then((r) => r.text())
 
-    await nodeHtmlToImage({
-      output: './image.png',
-      html,
-    })
+    const fileName = `./screenshots/${url.hashCode()}.png`
 
-    const palette = await vibrant.from('./image.png').getPalette()
+    if (!fs.existsSync(fileName)) {
+      await captureWebsite.file(url, fileName, {
+        fullPage: true,
+      })
+    }
+
+    const palette = await vibrant.from(fileName).getPalette()
 
     return {statusCode: 200, headers, body: JSON.stringify({palette})}
   } catch (error) {
