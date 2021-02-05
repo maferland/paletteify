eb83815c1aa25932cd63b6f13dd1890a290097ed
Marc-Antoine Ferland <maferland@hey.com>
2021-02-05 14:53:11 -0500

    feat: start styling page

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,14 +1,27 @@
 import Footer from './footer'
+import styled from '@emotion/styled'
 
 type LayoutProps = {
   children: React.ReactElement
 }
 
+const Page = styled.div`
+  background: var(--bg-color);
+  color: var(--dark-blue);
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
+
+const Main = styled.main`
+  flex: 1;
+`
+
 export default function Layout({children}: LayoutProps) {
   return (
-    <div>
-      <main>{children}</main>
+    <Page>
+      <Main>{children}</Main>
       <Footer />
-    </div>
+    </Page>
   )
 }
