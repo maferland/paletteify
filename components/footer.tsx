a433db2f1d140658a80cfff01abe2fe5226d7e3e
Marc-Antoine Ferland <maferland@hey.com>
2021-02-08 21:30:17 -0500

    feat: style footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,25 @@
+import styled from '@emotion/styled'
+
+const Container = styled.footer`
+  text-align: center;
+  color: white;
+  font-family: monospace;
+  font-size: 1.5em;
+  padding: 0.25em;
+
+  a {
+    text-decoration: underline;
+    color: white;
+  }
+`
+
 export default function Footer() {
   const year = new Date().getFullYear().toString()
 
   return (
-    <footer>
+    <Container>
       © Built with &lt;3 by <a href="https://maferland.com">maferland</a> -{' '}
       {year}
-    </footer>
+    </Container>
   )
 }
