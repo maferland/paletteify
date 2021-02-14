99a89eb93c369424cc1f40cd548c134d20a4d025
Marc-Antoine Ferland <maferland@hey.com>
2021-02-14 17:26:42 -0500

    feat: add social sharing meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <title>Paletteify</title>
+        <meta property="og:title" content="Paletteify" />
+        <meta
+          property="og:description"
+          content="Generate a color palette from most URL"
+        />
+        <meta
+          property="og:image"
+          content="https://paletteify.maferland.com/preview.jpg"
+        />
+        <meta property="og:url" content="http://paletteify.maferland.com" />
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
